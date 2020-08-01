29d62bac13c131531a9617cc59bace1132bc0707
syed zain gilani <syedzainrock8@gmail.com>
2020-08-01 18:37:25 +0500

    zain1222

--- supporting_material/zainproject/create.sh
+++ supporting_material/zainproject/create.sh
@@ -1,5 +1,5 @@
 aws cloudformation create-stack \
 --stack-name $1 \
---template-body file://$2 \ 
+--template-body file://$2 \
 --parameters file://$3 \
---capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-west-
+--capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-west-2
